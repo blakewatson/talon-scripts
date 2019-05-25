f8e4fa3e007c996aafea053ea766df02e563f4b4
Blake Watson <blake@blakewatson.com>
2019-05-24 20:31:23 -0500

    added commands for quicker scrolling

--- blake.py
+++ blake.py
@@ -70,6 +70,8 @@ ctx.keymap({
     'up (1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9)': multi_arrow,
     'right (1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9)': multi_arrow,
     'down (1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9)': multi_arrow,
+    'trooper': [Key('up')] * 9,
+    'cooper': [Key('down')] * 9,
 
     # code
     # 'state far': 'var ',
