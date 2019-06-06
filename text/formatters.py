b89257f031f3b98f84e546f4fd47445c34e09e33
Blake Watson <blake@blakewatson.com>
2019-06-06 15:06:37 -0500

    added the more command

--- text/formatters.py
+++ text/formatters.py
@@ -115,6 +115,7 @@ ctx = Context("formatters")
 ctx.keymap(
     {
         "(phrase | say) <dgndictation> [over]": text,
+        "more <dgndictation> [over]": [" ", spoken_text],
         "sentence <dgndictation> [over]": sentence_text,
         "(comma | ,) <dgndictation> [over]": [", ", spoken_text],
         "period <dgndictation> [over]": [". ", sentence_text],
