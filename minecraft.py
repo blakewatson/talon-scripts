8f926283988948df23d2a80ef4218ca2a8e4bee6
Blake Watson <blake@blakewatson.com>
2021-08-01 01:47:59 -0500

    minecraft script fixes

--- minecraft.py
+++ minecraft.py
@@ -1,12 +1,6 @@
-from talon import noise, actions, Context
-from time import sleep, time
+from talon import noise, actions, ui
+from time import time
 from threading import Timer
-
-ctx = Context()
-ctx.matches = r"""
-app: java
-and title: /Minecraft/
-"""
 
 is_active = False
 start = 0
@@ -21,6 +15,12 @@ def start_hiss_action():
 
 def on_hiss(active):
   global is_active, start, noise_length, t
+  
+  if 'Minecraft' not in ui.active_window().title:
+    return
+
+  if 'java' != ui.active_app().name:
+    return
 
   if active:
     is_active = True
